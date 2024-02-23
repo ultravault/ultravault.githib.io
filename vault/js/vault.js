4acc40693083f3d15855c66f6cca52a2a38e024b
Kwame Adoko <blakroku@gmail.com>
2024-02-23 02:02:22 +0100

    Full demo commit

---
+++ vault/js/vault.js
@@ -0,0 +1,88 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        vaultNumber: null,
+        // apiUrl: 'https://clx.frb.io/api/vaults/verify/',
+        apiUrl: 'http:127.0.0.1:3003/api/vaults/verify/',
+        profile: [],
+        vault: [],
+        address: [],
+        errorMessage: null,
+    },
+    methods: {
+        submitForm () {
+            if(this.validateForm()) {
+                this.processForm();
+            } else {
+                document.getElementById('error').className = 'block text-red-700 mb-6 tracking-widest'
+                document.getElementById('error').innerHTML = 'Reference or Security code required';
+            }
+
+        },
+
+        validateForm () {
+           if (this.vaultNumber) {
+               return true;
+           }
+        },
+
+        processForm () {
+            document.getElementById('loading').className = 'block tracking-widest text-green-700 mb-6';
+            document.getElementById('error').className = 'hidden';
+
+            // console.log(this.apiUrl  + this.vaultNumber)
+            
+            axios.get(this.apiUrl + this.vaultNumber)
+            
+                .then((res) => {
+
+                    document.getElementById('loading').className = 'hidden';
+
+                    if(!res.data.success) {
+                        this.errorMessage = res.data.message
+                        document.getElementById('error').className = 'block text-red-700 mb-6 tracking-widest'
+                        document.getElementById('error').innerHTML = this.errorMessage
+
+                        // console.log('error has happened');
+
+                    } 
+
+                    if(res.data.data.success) {
+                        
+                        console.log(res.data.data.business_profile + ' true-true');
+                    
+
+                        this.profile = res.data.data.business_profile;
+                        this.vault = res.data.data.vault;
+                        this.address = res.data.data.vault.address;
+
+                        // console.log(res);
+
+                        // document.getElementById('error').className = 'hidden'
+                    
+                        document.getElementById('error').className = 'block text-green-700 mb-6 tracking-widest'
+                        document.getElementById('error').innerHTML = 'Success. Redirecting in 5 seconds...'
+
+                        setTimeout(function () {
+                            
+                            document.getElementById('login-form').className = 'hidden'
+                            
+                            document.getElementById('result-container').className = 'block'
+                        
+                        }, 4000);
+                    }
+
+                
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+            .then(function () {
+              // always executed
+            });
+
+            // return false
+        }
+
+    }
+});
---
+++ wp-content/themes/ultra-vault/assets/js/masonry.init.min001e.js
@@ -0,0 +1,8 @@
+/*
+* webimp-client-theme - v1.0.0 - 2017-12-07
+* https://www.webimp.com.sg/solutions/web-design-development/
+* Copyright (c) 2017 Khairul Anwar <khairul@webimp.com.sg>
+*/
+
+
+!function(o){o(document).ready(function(){var n=o(".masonry-grid").masonry({itemSelector:".masonry-grid-item",columnWidth:".masonry-grid-item",percentPosition:!0});n.imagesLoaded().progress(function(){n.masonry("layout")})})}(jQuery);
